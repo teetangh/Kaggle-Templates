f276cabc6f225c461f1914bd9ef9fda295d05ecf
Kaustav Ghosh <teetangh@gmail.com>
2021-03-09 10:13:42 +0530

    started first kaggle notebook

---
+++ Tabular Playground Series - Feb 2021/train.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    df_train = pd.read_csv("Tabular Playground Series - Feb 2021/data/train.csv")
+    print(df_train)
+
+if __name__ == '__main__':
+    main()
